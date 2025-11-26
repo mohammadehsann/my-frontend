e711bd853e6095c6051a07b615a8c2def3d6fd77
mohammad ehsan nazari <nazariehsan80@gmail.com>
2025-11-26 16:04:35 +0330

    for authorization of owners post

--- src/pages/Feed/SinglePost/SinglePost.js
+++ src/pages/Feed/SinglePost/SinglePost.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-
 import Image from "../../../components/Image/Image";
 import "./SinglePost.css";
 const API = "https://my-backend-vnhm.onrender.com";
+
 class SinglePost extends Component {
   state = {
     title: "",
@@ -10,6 +10,7 @@ class SinglePost extends Component {
     date: "",
     image: "",
     content: "",
+    creatorId: "", // Add this to store post creator's ID
   };
 
   componentDidMount() {
@@ -30,6 +31,7 @@ class SinglePost extends Component {
           image: `${API}/` + resData.post.imageUrl,
           date: new Date(resData.post.createdAt).toLocaleDateString("en-US"),
           content: resData.post.content,
+          creatorId: resData.post.creator._id, // Store creator ID
         });
       })
       .catch((err) => {
@@ -37,13 +39,55 @@ class SinglePost extends Component {
       });
   }
 
+  editHandler = () => {
+    // Navigate to edit page or open edit modal
+    this.props.history.push(`/edit-post/${this.props.match.params.postId}`);
+  };
+
+  deleteHandler = () => {
+    if (window.confirm("Are you sure you want to delete this post?")) {
+      fetch(`${API}/feed/post/` + this.props.match.params.postId, {
+        method: "DELETE",
+        headers: { Authorization: "Bearer " + this.props.token },
+      })
+        .then((res) => {
+          if (res.status !== 200 && res.status !== 201) {
+            throw new Error("Deleting post failed!");
+          }
+          return res.json();
+        })
+        .then((resData) => {
+          console.log(resData);
+          this.props.history.push("/");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+
   render() {
+    const canEdit = this.state.creatorId === this.props.userId;
+
     return (
       <section className="single-post">
         <h1>{this.state.title}</h1>
         <h2>
           Created by {this.state.author} on {this.state.date}
         </h2>
+
+        {/* Show edit/delete buttons only to post owner */}
+        {canEdit && (
+          <div className="single-post__actions">
+            <button className="btn" onClick={this.editHandler}>
+              Edit
+            </button>
+            <button className="btn btn-danger" onClick={this.deleteHandler}>
+              Delete
+            </button>
+          </div>
+        )}
+
         <div className="single-post__image">
           <Image contain imageUrl={this.state.image} />
         </div>
